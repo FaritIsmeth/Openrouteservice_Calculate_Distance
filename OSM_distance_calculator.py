208527a551d76c5e371d6518f741319ce389da9e
Farit Ismeth Bin Ali <faritismeth22@gmail.com>
2021-12-29 17:19:28 +0800

    Update OSM_distance_calculator.py

--- OSM_distance_calculator.py
+++ OSM_distance_calculator.py
@@ -2,15 +2,14 @@ import requests
 import pandas
 import time
 
-df = pandas.read_csv('[POI Collection_Maintenance]_WS_V.1_[GFG POIs Review] - Sheet39.csv',dtype=str)
-#df_length = len(df)
-df_length=869
+df = pandas.read_csv('input.csv',dtype=str)
+df_length = len(df)
 get_source_coordinates={}
 get_onemap_coordinates={}
 counter=buffer_size=counter_iteration=0
 timer=5
 iteration_timer=60
-api_key="5b3ce3597851110001cf62480dccb722eac64489bf31f70666586529"
+api_key=""
 
 headers = {
     'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
